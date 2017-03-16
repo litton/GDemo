41a0576322af8e406be0edd87497ca9ab7963c7e
fanlitao <fanlitao@baidu.com>
2017-03-16 20:52:32 +0800

    添加超级管理员

--- GDemo/app/src/main/java/com/fantasy/coolgif/main/SettingActivity.java
+++ GDemo/app/src/main/java/com/fantasy/coolgif/main/SettingActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v4.text.TextUtilsCompat;
 import android.support.v4.view.ViewPager;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.fantasy.coolgif.R;
+import com.fantasy.coolgif.utils.PreferenceUtil;
+import com.fantasy.coolgif.utils.Utils;
 
 import java.util.List;
 
@@ -29,6 +34,25 @@ public class SettingActivity extends Activity implements View.OnClickListener {
 
         findViewById(R.id.like_gif).setOnClickListener(this);
         findViewById(R.id.heart_gif).setOnClickListener(this);
+
+        if(Utils.isSuperAccount()) {
+            findViewById(R.id.set_pos).setVisibility(View.VISIBLE);
+            final EditText posEditText = (EditText) findViewById(R.id.pos);
+            findViewById(R.id.confirm).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(!TextUtils.isEmpty(posEditText.getText())) {
+                        String postition = (String)posEditText.getText().toString();
+
+                        int pos =  Integer.parseInt(postition);
+                        PreferenceUtil.saveGifCurrentPos(pos);
+                    }
+                }
+            });
+
+        }
+
     }
 
 
